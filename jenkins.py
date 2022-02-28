03e65f27a7d18e306961c452124c8b30e3d379a6
sngtmth <90056427+sngtmth@users.noreply.github.com>
2022-02-28 14:09:38 +0530

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -1,21 +1,19 @@
 pipeline {
-    agent any
-
-    stages {
-        stage('Build') {
-            steps {
-                echo 'Building..'
-            }
-        }
-        stage('Test') {
-            steps {
-                echo 'Testing..'
-            }
-        }
-        stage('Deploy') {
-            steps {
-                echo 'Deploying....'
-            }
-        }
-    }
+   agent any
+   stages {
+       stage('Build Code') {
+           steps {
+               sh """
+               echo "Building Artifact"
+               """
+           }
+       }
+      stage('Deploy Code') {
+          steps {
+               sh """
+               echo "Deploying Code"
+               """
+          }
+      }
+   }
 }
