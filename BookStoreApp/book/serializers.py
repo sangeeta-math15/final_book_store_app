2c9c3311d719c0573d2b34bc180f91f7dea6cae9
sngtmth <90056427+sngtmth@users.noreply.github.com>
2022-03-02 23:09:47 +0530

    Update serializers.py

--- BookStoreApp/book/serializers.py
+++ BookStoreApp/book/serializers.py
@@ -12,16 +12,4 @@ class BookSerializer(serializers.ModelSerializer):
         # all fields in the model should be used.
         fields = '__all__'
 
-    def create(self, validate_data):
-        """
-        for creating the books
-        :param validate_data: validating the api data
-        """
-        books = Book.objects.create(
-            author=validate_data.get("author"),
-            title=validate_data.get("title"),
-            quantity=validate_data.get("quantity"),
-            price=validate_data.get("price"),
-            description=validate_data.get("description")
-        )
-        return books
+    
